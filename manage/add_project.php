c1982caf256a87993ab7c594d1e5094294733ebe
Phương <trananhphuong83@gmail.com>
2018-03-30 09:09:40 +0700

    Project editing feature has been added

--- manage/add_project.php
+++ manage/add_project.php
@@ -1,59 +1,93 @@
 <?php
-/*=======================================================\
-|                        FrontKanban                     |
-|--------------------------------------------------------|
-|   Creator: Phương                                      |
-|   Date :   01-Dec-2017                                 |
-|   Description: Frontaccounting Project Management Ext  |
-|   Free software under GNU GPL                          |
-|                                                        |
-\=======================================================*/
+/*======================================================\
+|                      FrontKanban                      |
+|-------------------------------------------------------|
+|  Creator: Phương                                      |
+|  Date :   01-Dec-2017                                 |
+|  Description: Frontaccounting Project Management Ext  |
+|  Free software under GNU GPL                          |
+|                                                       |
+\======================================================*/
 $page_security = 'SA_MANAGER';
 $path_to_root  = '../../..';
 
-include_once($path_to_root . "/includes/session.inc");
+include_once($path_to_root . '/includes/session.inc');
 add_access_extensions();
 
 $js = "";
 if (user_use_date_picker())
 	$js .= get_js_date_picker();
 
-include_once($path_to_root . "/includes/ui.inc");
-include_once($path_to_root . "/modules/kanban/includes/kanban_db.inc");
-include_once($path_to_root . "/modules/kanban/includes/kanban_ui.inc");
+include_once($path_to_root . '/includes/ui.inc');
+include_once($path_to_root . '/modules/kanban/includes/kanban_db.inc');
+include_once($path_to_root . '/modules/kanban/includes/kanban_ui.inc');
 
-page(_($help_context = "Create Project"), false, false, "", $js);
+page(_($help_context = 'Manage Project'), false, false, '', $js);
+
+if(isset($_GET['ID']))
+	$selected_id = $_GET['ID'];
+elseif(isset($_POST['selected_id']))
+    $selected_id = $_POST['selected_id'];
 
 //-----------------------------------------------------------------------------
 
 if(isset($_POST['addupdate'])) {
-	if(strlen($_POST['proj_name']) == 0 || $_POST['proj_name'] == "") {
-		display_error(_("The project name must be entered."));
+	if(strlen($_POST['proj_name']) == 0 || $_POST['proj_name'] == '') {
+		display_error(_('Project name must be entered.'));
 		set_focus('proj_name');
 	}
 	else {
-		write_project(false, $_POST['proj_name'], get_post('desc'), null, Today(), null, null, null, $_SESSION["wa_current_user"]->user);
-		$id = db_insert_id();
-		if(add_project_data_file($id))
-		    display_notification(_('New project created'));
-		else{
-			delete_project($id);
-			display_error(_('could not created data file'));
+		write_project(@$selected_id, get_post('proj_name'), get_post('proj_description'), get_post('proj_type'), Today(), null, $_POST['begin_date'], $_POST['end_date'], $_SESSION['wa_current_user']->user, check_value('closed'));
+
+		if(empty($selected_id)) {
+            $id = db_insert_id();
+		    if(add_project_data_file($id))
+		        display_notification(_('New project created'));
+		    else {
+			    delete_project($id);
+			    display_error(_('Could not create data file'));
+		    }
 		}
+		else
+            display_notification(_('Project has been updated.'));
 	}
+	$sav = get_post('selected_id');
+	unset($_POST);
+	$_POST['selected_id'] = $sav;
+	$Ajax -> activate('_page_body');
 }
 
 //-----------------------------------------------------------------------------
 
 start_form();
-start_table(TABLESTYLE_NOBORDER);
-text_row(_("Project name:"), 'proj_name', get_post('proj_name'), 40, 50);
-textarea_row(_("Description:"), 'desc', null, 36, 5);
+
+if(!empty($selected_id)) {
+	$proj = get_projects($selected_id);
+	$_POST['proj_name'] = $proj['proj_name'];
+	$_POST['proj_type'] = $proj['proj_type'];
+	$_POST['begin_date'] = sql2date($proj['begin_date']);
+	$_POST['end_date'] = sql2date($proj['end_date']);
+	$_POST['proj_description'] = $proj['proj_description'];
+	$_POST['closed'] = $proj['closed'];
+
+	hidden('selected_id', $selected_id);
+}
+
+start_table(TABLESTYLE2);
+text_row(_('Project Name').':', 'proj_name', null, 40, 50);
+project_types_radio_row(_('Project Type').':', 'proj_type', get_post('proj_type'));
+date_row(_('Begin Date').':', 'begin_date');
+date_row(_('End Date').':', 'end_date', null, null, 0, 0, 1);
+textarea_row(_('Description').':', 'proj_description', null, 36, 5);
+check_row(_('Closed').':', 'closed');
 end_table(1);
 
 div_start('controls');
-	submit_center('addupdate', _("Add New Project"), true, '', 'default');
+
+$txt = empty($selected_id) ? _('Add New Project') : _('Update Project');
+submit_center('addupdate', $txt, true, '', 'default');
+
 div_end();
 
 end_form(1);
-end_page(true, true, false, 3);
+end_page();
--- manage/projects.php
+++ manage/projects.php
@@ -1,13 +1,13 @@
 <?php
-/*=======================================================\
-|                        FrontKanban                     |
-|--------------------------------------------------------|
-|   Creator: Phương                                      |
-|   Date :   01-Dec-2017                                 |
-|   Description: Frontaccounting Project Management Ext  |
-|   Free software under GNU GPL                          |
-|                                                        |
-\=======================================================*/
+/*======================================================\
+|                      FrontKanban                      |
+|-------------------------------------------------------|
+|  Creator: Phương                                      |
+|  Date :   01-Dec-2017                                 |
+|  Description: Frontaccounting Project Management Ext  |
+|  Free software under GNU GPL                          |
+|                                                       |
+\======================================================*/
 
 $page_security = 'SA_MANAGER';
 $path_to_root  = '../../..';
@@ -38,41 +38,67 @@ function check_overdue($row) {
 function get_owner($row) {
 	return get_user($row['owner_id'])['real_name'];
 }
-
-function project_list() {
-    $sql = get_projects();
-
-        $cols = array(
-          _('ID') => '',
-          _('Name') => array('fun'=>'project_link'),
-		  _('Description'),
-          _('Type'),
-          _('Created date') => array(),
-          _('Closed date') => array(),
-          _('Begin date') => array(),
-          _('End date') => array(),
-          _('Owner') => array('fun'=>'get_owner')
-        );
-
-        $table = new_db_pager('projects_tbl', $sql, $cols);
-		$table->set_marker('check_overdue', _("Marked rows are overdue."));
-        // $table->width = "60%";
-	
-	    display_note(_('Press name to go to project details.'));
-        display_db_pager($table);
+function get_project_type($row) {
+    return $row['proj_type'] == 0 ? _('Public') : _('Private');
+}
+function edit_link($row) {
+    return button('edit_'.$row['proj_id'], _('Edit this project'), false, ICON_EDIT);
 }
 
 //--------------------------------------------------------------------------
 
-if(isset($_GET['action']) && $_GET['action'] == 'list') {
+foreach (db_query(get_projects(false, true)) as $proj) {
+    if(isset($_POST['edit_'.$proj['proj_id']]))
+        meta_forward($path_to_root.'/modules/kanban/manage/add_project.php', 'ID='.$proj['proj_id']);
+}
+
+if(empty($_GET['proj'])) {
+
     page(_($help_context = "Projects List"), false, false, "", $js);
-    project_list();
+    start_form();
+
+    start_outer_table(TABLESTYLE2, "cellpadding='10'");
+    table_section(1);
+    check_row(_('Also Closed').':', 'closed', null, true);
+    table_section(2);
+    check_row(_('Only Private').':', 'only_private', null, true);
+    table_section(3);
+    check_row(_('Only Public').':', 'only_public', null, true);
+    end_outer_table(1);
+    
+    if(check_value('only_public') == 1 && check_value('only_private') != 1)
+        $type = 0;
+    elseif(check_value('only_public') != 1 && check_value('only_private') == 1)
+        $type = 1;
+    else
+        $type = false;
+
+    $sql = get_projects(false, check_value('closed'), $type);
+
+    $cols = array(
+        _('ID') => array('align'=>'center'),
+        _('Name') => array('fun'=>'project_link'),
+        _('Description'),
+        _('Type') => array('fun'=>'get_project_type', 'align'=>'center'),
+        _('Created date') => array('type'=>'date'),
+        _('Closed date') => array('type'=>'date'),
+        _('Begin date') => array('type'=>'date'),
+        _('End date') => array('type'=>'date'),
+        _('Owner') => array('fun'=>'get_owner'),
+        _('Edit') => array('fun'=>'edit_link','align'=>'center')
+    );
+
+    $table = new_db_pager('projects_tbl', $sql, $cols);
+    $table->set_marker('check_overdue', _("Marked rows are overdue."));
+    // $table->width = "60%";
+    display_note(_('Press name to go to project details.'));
+    display_db_pager($table);
+
+    end_form();
 }
 else {
-	if(empty($_GET['proj']))
-        $_SESSION['project'] = get_last_project()[0];
-    else
-        $_SESSION['project'] = $_GET['proj'];
+
+    $_SESSION['project'] = $_GET['proj'];
 
     page($help_context = get_projects($_SESSION['project'])['proj_name'], false, false, "", $js);
 
