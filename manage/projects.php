77dc49d4d07cfbaac77eee4ee906ab638712cfa9
Phuong <trananhphuong83@gmail.com>
2019-09-16 08:54:13 +0700

    Increase the length of projects list page

--- manage/projects.php
+++ manage/projects.php
@@ -11,32 +11,32 @@
 
 $page_security = 'SA_MANAGER';
 $path_to_root  = '../../..';
-include($path_to_root . "/includes/db_pager.inc");
-include_once($path_to_root . "/includes/session.inc");
+include($path_to_root.'/includes/db_pager.inc');
+include_once($path_to_root.'/includes/session.inc');
 add_access_extensions();
 
-$js = "";
+$js = '';
 if ($SysPrefs->use_popup_windows)
-	$js .= get_js_open_window(700, 300);
+    $js .= get_js_open_window(700, 300);
 if (user_use_date_picker())
-	$js .= get_js_date_picker();
+    $js .= get_js_date_picker();
 
-include_once($path_to_root . "/includes/ui.inc");
-include_once($path_to_root . "/modules/kanban/includes/kanban_db.inc");
-include_once($path_to_root . "/modules/kanban/includes/kanban_ui.inc");
+include_once($path_to_root.'/includes/ui.inc');
+include_once($path_to_root.'/modules/kanban/includes/kanban_db.inc');
+include_once($path_to_root.'/modules/kanban/includes/kanban_ui.inc');
 
 //--------------------------------------------------------------------------
 
 unset($_SESSION['project']);
 
 function project_link($row) {
-	return "<a href='?proj=".$row['proj_id']."'>".$row['proj_name']."</a>";
+    return "<a href='?proj=".$row['proj_id']."'>".$row['proj_name']."</a>";
 }
 function check_overdue($row) {
-	return false;
+    return false;
 }
 function get_owner($row) {
-	return get_user($row['owner_id'])['real_name'];
+    return get_user($row['owner_id'])['real_name'];
 }
 function get_project_type($row) {
     return $row['proj_type'] == 0 ? _('Public') : _('Private');
@@ -57,7 +57,7 @@ foreach (db_query(get_projects(false, true)) as $proj) {
 
 if(empty($_GET['proj'])) {
 
-    page(_($help_context = "Projects List"), false, false, "", $js);
+    page(_($help_context = 'Projects List'), false, false, '', $js);
     start_form();
 
     start_outer_table(TABLESTYLE2, "cellpadding='10'");
@@ -92,8 +92,8 @@ if(empty($_GET['proj'])) {
     );
 
     $table = new_db_pager('projects_tbl', $sql, $cols);
-    $table->set_marker('check_overdue', _("Marked rows are overdue."));
-    // $table->width = "60%";
+    $table->set_marker('check_overdue', _('Marked rows are overdue.'));
+    $table->page_len = 20;
     display_note(_('Press name to go to project details.'));
     display_db_pager($table);
 
@@ -105,6 +105,6 @@ else {
 
     page($help_context = get_projects($_SESSION['project'])['proj_name'], false, false, "", $js);
 
-    include_once("$path_to_root/modules/kanban/includes/ui/board.inc");
+    include_once($path_to_root.'/modules/kanban/includes/ui/board.inc');
 }
 end_page();
