aac56bccad90a98652c464c1d88354045d7d637b
Tran Anh Phuong <trananhphuong83@gmail.com>
2021-03-15 09:33:22 +0700

    code cleanup

--- data.php
+++ data.php
@@ -16,7 +16,7 @@ include_once($path_to_root . "/modules/kanban/includes/PHPMailer/PHPMailerAutolo
 add_access_extensions();
 
 if(!file_exists(company_path().'/kanban_data'))
-    mkdir(company_path().'/kanban_data');
+	mkdir(company_path().'/kanban_data');
 
 define('DATA_FILE', company_path().'/kanban_data/'.$_SESSION['project']);
 
@@ -24,9 +24,9 @@ function save($data) {
 	// $encoded = json_encode($data, JSON_PRETTY_PRINT);
 	$encoded = preg_replace("/&quot;/", '"', $data);
 	$f = fopen(DATA_FILE, 'w') or die ("could not open file");
-    flock($f, LOCK_EX); //lock file to handle multi access at the same time
+	flock($f, LOCK_EX); //lock file to handle multi access at the same time
 	fwrite($f, $encoded);
-    flock($f, LOCK_UN); // release lock
+	flock($f, LOCK_UN); // release lock
 	fclose($f);
 }
 
@@ -39,46 +39,46 @@ function get_all_users() {
 	$result = array();
 	
 	foreach(get_users() as $row) {
-	    $result[] = $row;
+		$result[] = $row;
 	};
-    $result[] = $_SESSION['wa_current_user']->username;
-    $result[] = $_SESSION['wa_current_user']->name;
-    $result[] = $_SESSION['wa_current_user']->email;
-    echo json_encode($result);
+	$result[] = $_SESSION['wa_current_user']->username;
+	$result[] = $_SESSION['wa_current_user']->name;
+	$result[] = $_SESSION['wa_current_user']->email;
+	echo json_encode($result);
 }
 function sendEmail($data) {
-    
-    $host = get_company_pref('smtp_host');
-    $port = get_company_pref('smtp_port');
-    $user = get_company_pref('smtp_username');
-    $pass = get_company_pref('smtp_password');
-    $secure = get_company_pref('smtp_secure');
-    $sender = get_company_pref('smtp_sendername');
-    
-    $mail = new PHPMailer;
-    $mail->IsHTML(true);
-    $mail->CharSet = "text/html; charset=UTF-8;";
-    $mail->isSMTP();
-    $mail->SMTPDebug = 2;
-    $mail->Debugoutput = 'html';
-    $mail->Host = $host;
-    $mail->Port = $port;
-    $mail->SMTPSecure = $secure;
-    $mail->SMTPAuth = true;
-    $mail->Username = $user;
-    $mail->Password = $pass;
-    $mail->setFrom($user, $sender);
-    $mail->addReplyTo('replyto@example.com', 'No Reply');
-    $mail->addAddress($data['recipient'], 'recipient');
-    $mail->Subject = $data['subject'];
-    $mail->Body = html_entity_decode($data['content']);
-    $mail->AltBody = 'This is a plain-text message body';
-    if (!$mail->send()) {
-        echo "Mailer Error: " . $mail->ErrorInfo;
-    }
-    else {
-        echo json_encode($data);
-    }
+	
+	$host = get_company_pref('smtp_host');
+	$port = get_company_pref('smtp_port');
+	$user = get_company_pref('smtp_username');
+	$pass = get_company_pref('smtp_password');
+	$secure = get_company_pref('smtp_secure');
+	$sender = get_company_pref('smtp_sendername');
+	
+	$mail = new PHPMailer;
+	$mail->IsHTML(true);
+	$mail->CharSet = "text/html; charset=UTF-8;";
+	$mail->isSMTP();
+	$mail->SMTPDebug = 2;
+	$mail->Debugoutput = 'html';
+	$mail->Host = $host;
+	$mail->Port = $port;
+	$mail->SMTPSecure = $secure;
+	$mail->SMTPAuth = true;
+	$mail->Username = $user;
+	$mail->Password = $pass;
+	$mail->setFrom($user, $sender);
+	$mail->addReplyTo('replyto@example.com', 'No Reply');
+	$mail->addAddress($data['recipient'], 'recipient');
+	$mail->Subject = $data['subject'];
+	$mail->Body = html_entity_decode($data['content']);
+	$mail->AltBody = 'This is a plain-text message body';
+	if (!$mail->send()) {
+		echo "Mailer Error: " . $mail->ErrorInfo;
+	}
+	else {
+		echo json_encode($data);
+	}
 }
 
 if (function_exists($_POST['action'])) {
